c6e0a0b9dd5764e10e720d3535dc2c868a433657
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2022-02-08 11:32:18 +0100

    Remove old config params

--- runtime/config.go
+++ runtime/config.go
@@ -4,7 +4,6 @@ import (
 	"io/ioutil"
 	"path/filepath"
 
-	"github.com/wmnsk/go-gtp/gtpv1"
 	"gopkg.in/yaml.v3"
 )
 
@@ -21,79 +20,6 @@ func ParseConf(file string) error {
 	return nil
 }
 
-type FTEID struct {
-	TEID      uint32 `yaml:"teid"`
-	IPAddress string `yaml:"ipAddress"`
-}
-
-type SDFFilter struct {
-	FlowDescription string `yaml:"description"`
-}
-
-type UEIPAddress struct {
-	IPAddress string `yaml:"ip-address"`
-}
-
-type PDI struct {
-	SourceInterface string       `yaml:"sourceInterface"`
-	FTEID           *FTEID       `yaml:"fteid,omitempty"` // not required if source is not encapsulated
-	SDFFilter       *SDFFilter   `yaml:"sdf-filter,omitempty"`
-	UEIPAddress     *UEIPAddress `yaml:"ue-ip-address,omitempty"`
-}
-
-type PDR struct {
-	ID                 uint32              `yaml:"id"`
-	PDI                *PDI                `yaml:"pdi"`
-	Precedence         uint32              `yaml:"precedence"`
-	FARID              uint32              `yaml:"farid"`
-	OuterHeaderRemoval *OuterHeaderRemoval `yaml:"outerHeaderRemoval,omitempty"`
-}
-
-type OuterHeaderRemoval struct {
-	description             uint32
-	extensionHeaderDeletion uint32
-}
-
-type PDRs []*PDR
-
-func (pdrs PDRs) Len() int {
-	return len(pdrs)
-}
-func (pdrs PDRs) Less(i, j int) bool {
-	// element with highest precedence (lowest value in Precedence IE) should be sorted first
-	return pdrs[i].Precedence < pdrs[j].Precedence
-}
-
-func (pdrs PDRs) Swap(i, j int) {
-	pdrs[i], pdrs[j] = pdrs[j], pdrs[i]
-}
-
-type OuterHeaderCreation struct {
-	TEID     uint32 `yaml:"teid"`
-	GTPUPeer string `yaml:"gtpuPeer"`
-	uConn    *gtpv1.UPlaneConn
-}
-
-type ForwardingParameters struct {
-	DestinationInterface string               `yaml:"destinationInterface"`
-	OuterHeaderCreation  *OuterHeaderCreation `yaml:"outerHeaderCreation,omitempty"`
-}
-
-type ApplyAction struct {
-	Action string `yaml:"action"`
-}
-
-type FAR struct {
-	ID                   uint32                `yaml:"id"`
-	ApplyAction          *ApplyAction          `yaml:"applyAction"`
-	ForwardingParameters *ForwardingParameters `yaml:"forwardingParameters,omitempty"`
-}
-
-type PFCPSession struct {
-	PDRS []*PDR `yaml:"pdrs"`
-	FARS []*FAR `yaml:"fars"`
-}
-
 type DNN struct {
 	Dnn  string `yaml:"dnn"`
 	Cidr string `yaml:"cidr"`
@@ -108,6 +34,5 @@ type UpfConfig struct {
 	PFCPAddress          *string  `yaml:"pfcp-address,omitempty"`
 	GTPUProtocolEntities []string `yaml:"gtpu-entities"`
 	DNNList              []*DNN   `yaml:"dnn_list,omitempty"`
-	//PFCPSessions         []*PFCPSession `yaml:"pfcp-sessions"`
-	SimulateRAN *RAN `yaml:"simulate-ran,omitempty"`
+	SimulateRAN          *RAN     `yaml:"simulate-ran,omitempty"`
 }
